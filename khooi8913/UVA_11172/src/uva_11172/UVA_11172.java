17b8dd3a8b5f9c8198a7185c71247f5d417ea337
Nicholas Khooi <khooi8913@gmail.com>
2016-08-10 01:24:53 +0800

    Wed Aug 10 01:24:52 MPST 2016: update all by khooi8913

---
+++ khooi8913/UVA_11172/src/uva_11172/UVA_11172.java
@@ -0,0 +1,27 @@
+package uva_11172;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class UVA_11172 {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader systemInput=new BufferedReader(new InputStreamReader(System.in));
+        int numLines = Integer.parseInt(systemInput.readLine());
+        int i=0;
+        while(i<numLines)
+        {
+            StringTokenizer st = new StringTokenizer(systemInput.readLine());
+            double a = Double.parseDouble(st.nextToken());
+            double b = Double.parseDouble(st.nextToken());
+            if(a>b) System.out.println(">");
+            else if(a<b)    System.out.println("<");
+            else    System.out.println("=");
+            i++;
+        }
+    }
+    
+}
---
+++ khooi8913/UVA_11559/src/uva_11559/UVA_11559.java
@@ -0,0 +1,51 @@
+package uva_11559;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class UVA_11559 {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader systemInput=new BufferedReader(new InputStreamReader(System.in));
+        String a;
+        while((a=systemInput.readLine())!=null){
+            StringTokenizer st = new StringTokenizer(a);
+            int numParticipant = Integer.parseInt(st.nextToken());
+            int budget = Integer.parseInt(st.nextToken());
+            int hotels = Integer.parseInt(st.nextToken());
+            int weekends = Integer.parseInt(st.nextToken());
+            int cost=0;
+            int temp=0;
+            boolean OK = false;
+            for(int i=0;i<hotels;i++){
+                int hotelPrice = Integer.parseInt(systemInput.readLine());
+                String b = systemInput.readLine();
+                st = new StringTokenizer(b);
+                for(int j=0;j<weekends;j++){
+                    if(Integer.parseInt(st.nextToken())>=numParticipant){
+                        cost=numParticipant*hotelPrice;
+                        if(temp==0) {
+                            temp=cost;
+                        }
+                        else{
+                            if(cost<temp){
+                                temp=cost;
+                            }
+                        }
+                        if(temp<budget){
+                            OK = true;
+                        }
+                    }
+                }
+           }
+            if(OK)  System.out.println(temp);
+            else    System.out.println("stay home");
+        }
+            
+        
+    }
+    
+}
