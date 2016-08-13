3d6a8a5079164f5d6550676599fc0241874a06d4
Nicholas Khooi <khooi8913@gmail.com>
2016-08-14 01:14:20 +0800

    Sun Aug 14 01:14:20 MPST 2016: update all by khooi8913

---
+++ khooi8913/UVA_10783/src/uva_10783/UVA_10783.java
@@ -0,0 +1,28 @@
+package uva_10783;
+
+import java.util.Scanner;
+
+public class UVA_10783 {
+
+    public static void main(String[] args) {
+        
+        Scanner br = new Scanner(System.in);
+        int testCases = Integer.parseInt(br.nextLine());
+        for(int i=0;i<testCases;i++){
+            int starting = Integer.parseInt(br.nextLine());
+            int ending = Integer.parseInt(br.nextLine());
+            
+            if(starting%2==0)
+                starting++;
+            
+            int sum=0;
+            
+            for(int j=starting;j<=ending;j+=2){
+                sum+=j;
+            }
+            
+            System.out.println("Case "+ (i+1)+": "+sum);
+        }
+    }
+    
+}
