9a97139fe4a329a90315196429d60b0e3847d89b
Nicholas Khooi <khooi8913@gmail.com>
2016-08-24 14:06:15 +0800

    Wed Aug 24 14:06:15 MPST 2016: update all by khooi8913

---
+++ khooi8913/UVA_00725/src/uva_00725/UVA_00725.java
@@ -0,0 +1,47 @@
+package uva_00725;
+
+import java.util.Scanner;
+
+public class UVA_00725 {
+
+    public static void main(String[] args) {
+        
+        Scanner sn = new Scanner(System.in);
+        int t=0;
+        while(sn.hasNext()){
+            int n = sn.nextInt();
+            if(n==0)    System.exit(0);
+            boolean status=false;
+            if(t>0) System.out.println("");
+            for(int fghij=1234; fghij<=98765/n;fghij++){
+                int abcde = fghij * n;
+                int tmp;
+                int used=0;
+                if(fghij<10000) used=1;
+                
+                tmp = abcde;
+                while(tmp>0){
+                    used |= (1 << (tmp%10));
+                    tmp/=10;
+                }
+                
+                tmp = fghij;
+                while(tmp>0){
+                    used |= (1 << (tmp%10));
+                    tmp/=10;
+                }
+                
+                if(used == (1<<10)-1){
+                    System.out.printf("%05d / %05d = %d\n", abcde, fghij, n);
+                    status=true;
+                }
+            }
+                            
+            if(!status){
+                System.out.printf("There are no solutions for %d.\n", n);
+            }
+            t++;
+        }
+    }
+    
+}
