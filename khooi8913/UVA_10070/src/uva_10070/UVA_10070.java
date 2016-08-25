d25a0e38bb9853a08b66abadc2ed485b8b6d2bc3
Nicholas Khooi <khooi8913@gmail.com>
2016-08-26 01:06:30 +0800

    Fri Aug 26 01:06:29 MPST 2016: update all by khooi8913

--- khooi8913/UVA_10070/src/uva_10070/UVA_10070.java
+++ khooi8913/UVA_10070/src/uva_10070/UVA_10070.java
@@ -1,11 +1,18 @@
 package uva_10070;
-//NOT SOLVED RTE
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class UVA_10070 {
-
+    
+    static final BigInteger bi4 = new BigInteger("4");
+    static final BigInteger bi15 = new BigInteger("15");
+    static final BigInteger bi55 = new BigInteger("55");
+    static final BigInteger bi100 = new BigInteger("100");
+    static final BigInteger bi400 = new BigInteger("400");
+    
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,7 +20,7 @@ public class UVA_10070 {
         int z=0;
         while((s=br.readLine())!=null && !s.isEmpty()){
             
-            long year = Long.parseLong(s);
+            BigInteger x = new BigInteger(s);
             
             if(z>0)
                 System.out.println("");
@@ -22,15 +29,15 @@ public class UVA_10070 {
             boolean huluculu = false;
             boolean bulukulu = false;
             
-            if( (year%4==0 && year%100!=0) || (year%400==0)){
-                System.out.println("This is a leap year.");
+            if( ((x.mod(bi4).equals(BigInteger.ZERO)) && !(x.mod(bi100).equals(BigInteger.ZERO))) || (x.mod(bi400).equals(BigInteger.ZERO))){
+                System.out.println("This is leap year.");
                 leapyear=true;
             }          
-            if(year%15==0){
+            if(x.mod(bi15).equals(BigInteger.ZERO)){
                 System.out.println("This is huluculu festival year.");
                 huluculu=true;
             }
-            if(leapyear && year%55==0){
+            if(leapyear && x.mod(bi55).equals(BigInteger.ZERO)){
                 System.out.println("This is bulukulu festival year.");
                 bulukulu=true;
             }
