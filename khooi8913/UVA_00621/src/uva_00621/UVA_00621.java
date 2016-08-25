dd21fe5c5df85c4372ffafa58993ead8a7b81262
Nicholas Khooi <khooi8913@gmail.com>
2016-08-25 14:08:42 +0800

    Thu Aug 25 14:08:41 MPST 2016: update all by khooi8913

---
+++ khooi8913/UVA_00621/src/uva_00621/UVA_00621.java
@@ -0,0 +1,28 @@
+package uva_00621;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UVA_00621 {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numCases = Integer.parseInt(br.readLine());
+        
+        for(int i=0;i<numCases;i++){
+            String s = br.readLine();
+            if(s.equals("1")||s.equals("4")||s.equals("78"))
+                System.out.println("+");
+            else if(s.charAt(s.length()-2)=='3'&&s.charAt(s.length()-1)=='5')
+                System.out.println("-");
+            else if(s.charAt(0)=='9'&&s.charAt(s.length()-1)=='4')
+                System.out.println("*");
+            else if(s.charAt(0)=='1'&&s.charAt(1)=='9'&&s.charAt(2)=='0')
+                System.out.println("?");
+        }
+        
+    }
+    
+}
