9dca38f19ae2ca750f691d5219f0b2a1e3cc472f
King <king8793yyk@hotmail.com>
2017-03-16 21:22:15 +0800

    Thu, Mar 16, 2017  9:22:14 PM: update all by king

--- King/00272 TEX Quotes.java
+++ King/00272 TEX Quotes.java
@@ -5,26 +5,22 @@ import java.io.InputStreamReader;
 class Main {
 
 	public static void main(String[] args)  throws IOException {  
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s;
-        boolean start=true;
+        	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        	String s;
+        	boolean start=true;
         
-        while ((s=br.readLine())!=null){
-            StringBuilder sb=new StringBuilder();
-        	char [] c=s.toCharArray();
-        	for (int i=0;i<c.length;i++) {
-        		if (c[i]=='\"') {
-        			if (start) {
-        				sb.append("``");
+        	while ((s=br.readLine())!=null){
+            		StringBuilder sb=new StringBuilder();
+        		char [] c=s.toCharArray();
+        		for (int i=0;i<c.length;i++) {
+        			if (c[i]=='\"') {
+					sb.append( start ? "``" : "''" );
+        				start=!start;
         			} else {
-        				sb.append("''");
+    					sb.append(c[i]);
         			}
-        			start=!start;
-        		} else {
-    				sb.append(c[i]);
         		}
-        	}
         	System.out.println(sb.toString());
-        }
+        	}
 	}  
 }
